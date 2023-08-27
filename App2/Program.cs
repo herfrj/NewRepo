602e4f5f130f147ef0a4dd2281d71a87b2f4f308
Oleksandr Zinoviev <sasha.zinovyev@gmail.com>
2023-08-27 13:05:03 +0300

    Added Task 1

---
+++ App2/Program.cs
@@ -0,0 +1,154 @@
+﻿//class Program
+//{
+
+//static void Main(string[] args)
+//  Task1{};
+
+
+//private static void Task1()
+//{
+
+/*
+byte var;
+byte num = 20;
+var = num += 5;
+Console.WriteLine($"Переменная: {var}");
+*/
+
+/*
+Console.Write("Enter four-digit number: ");
+int days = Convert.ToInt16(Console.ReadLine());
+const short year = 365; // number of days in a year
+const byte d_week = 7; // number of days in a week
+const byte n_week = 4; // number of weeks in a month
+
+if (days > 0)
+{
+    int a = days / year;
+    int m = ((days - a * year) / d_week) / n_week;
+    int d = (days - a * year) - (m * n_week * d_week);
+    Console.WriteLine($"It's {a} years, {m} month, {d} days");
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
+*/
+
+/*
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"n + n * 2 = {n + n * 2}");
+*/
+
+/*
+sbyte a = -34;
+byte b = 4;
+'Hello' // dunno obj doesn't work 
+char d = 'R';
+double e = 23.093433222;
+ushort f = 40000;
+bool g = true;
+byte h = 0;
+*/
+
+/*
+Console.Write("Enter short: ");
+try
+{
+    short a = Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine("Success");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
+Console.Write("Enter ulong: ");
+try
+{
+    ulong b = Convert.ToUInt64(Console.ReadLine());
+    Console.WriteLine("Success");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
+Console.Write("Enter char: ");
+try
+{
+    char c = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine("Success");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
+Console.Write("Enter double: ");
+try
+{
+    double d = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Success");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
+*/
+
+/*
+int a = -5;
+a *= 7;
+--a;
+Console.WriteLine(a);
+*/
+
+/*
+Console.WriteLine("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 2;
+if (b * 2 == a)
+    Console.WriteLine("Number is even");
+else
+    Console.WriteLine("Number is odd");
+*/
+
+/*
+Console.WriteLine("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 50 & a!=37 & a > 32 | a == 0 | a ==5)
+    Console.WriteLine("True");
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+/*
+Console.WriteLine("Enter fist number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter symbol (+, -, *, /): ");
+char c = Convert.ToChar(Console.ReadLine());
+
+switch (c)
+{
+    case '+':
+        Console.WriteLine(a + b); break;
+    case '-':
+        Console.WriteLine(a - b); break;
+    case '*':
+        Console.WriteLine(a * b); break;
+    case '/':
+        Console.WriteLine(a / b); break;
+}
+*/
+
+//}
+
+//}
+
+
+
+
+
